64eaa81170ca98aa5d26415761718f7c82839965
Kyle Davison <kdavison@gmail.com>
2017-02-20 21:31:04 -0600

    randomized output directory!

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BUILD=$(find * -type d -name "out.*")
+if [ -z "${BUILD}" ]
+then
+BUILD=$(mktemp -d --tmpdir=$(pwd) -t out.XXXXXXXX)
+else
+BUILD=$(pwd)/${BUILD}
+fi
+
+cd ${BUILD} && cmake ../
