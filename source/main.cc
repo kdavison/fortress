24df64b71dec1cf628a4ef222b9deeebd2f7dd78
Kyle Davison <kdavison@gmail.com>
2017-04-26 01:46:42 -0500

    holy shit I got it (nearly)

--- source/main.cc
+++ source/main.cc
@@ -12,6 +12,12 @@ int main()
     SDL2pp::Window window("libSDL2pp demo",
                 SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
                 640, 480, SDL_WINDOW_RESIZABLE);
+    SDL2pp::Renderer renderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Delay(2000);
+  }
+  catch(SDL2pp::Exception& ex)
+  {
+    console->error(ex.GetSDLError());
   }
   catch(std::exception& ex)
   {
